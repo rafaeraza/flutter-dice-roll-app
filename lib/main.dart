4f7c567765ea906022ff6346976a848286a25c18
Rafae Raza <rafae.raza@gmail.com>
2023-07-29 23:25:19 +0500

    refactor: update `main.dart` to display gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dice_roll_app/widgets/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,24 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Dice Roll App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Dice Roll App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'This is the beginning of my Dice Roll App!',
-              ),
-            ],
-          ),
+        body: GradientContainer(
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 104, 26, 237),
         ),
       ),
     );
